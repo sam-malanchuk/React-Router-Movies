982a6e0c35262abe67b8eda2f87cec789effa023
sam-malanchuk <malanchuksam@gmail.com>
2019-06-19 21:52:12 -0400

    Completed Stretch #2: now clicking save goes up to the saving function in App.js and adds it to the savedList state

--- client/src/App.js
+++ client/src/App.js
@@ -13,18 +13,25 @@ export default class App extends Component {
     };
   }
 
-  // addToSavedList = movie => {
-  //   const savedList = this.state.savedList;
-  //   savedList.push(movie);
-  //   this.setState({ savedList });
-  // };
+  addToSavedList = movie => {
+    const savedList = this.state.savedList;
+    savedList.push(movie);
+    this.setState({ savedList });
+  };
 
   render() {
     return (
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route path="/movies/:id" render={(props) => (
+  <Movie 
+    addToSavedList={this.addToSavedList} 
+    match={props.match}
+    history={props.history}
+    location={props.location}
+  />
+)} />
       </div>
     );
   }
