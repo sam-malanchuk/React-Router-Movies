1998488f4490eec463c137b96def7ca61279b7f3
sam-malanchuk <malanchuksam@gmail.com>
2019-06-22 12:30:39 -0400

    created the savedList into [NavLink]s

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default class SavedList extends Component {
   render() {
@@ -7,7 +7,7 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie" key={movie.id} >{movie.title}</span>
+          <NavLink to={`/movies/${movie.id}`} activeClassName="saved-active"><span className="saved-movie" key={movie.id} >{movie.title}</span></NavLink>
         ))}
         <Link to="/"><div className="home-button">Home</div></Link>
       </div>
